02fba881bd0a80025a1fd08a8421845a6c876635
danihs2 <daniel.hachac@alumno.buap.mx>
2023-10-12 01:02:17 -0600

    Modificacion a la pagina de inicio

--- next/app/ejercicios/page.jsx
+++ next/app/ejercicios/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useEffect } from 'react'
+import TextField from '@mui/material/TextField';
 import styles from './estilos.css'
 
 
@@ -14,15 +15,28 @@ export default function page() {
   
 
   return (
-    <div className='primer-block'>
-        <h1>Stay tuned for the <i className='parrafo-azul'>latest updates</i></h1>
-        <h2>on Alzheimer's recent discoveries</h2>
-        <p>Enter your email to be up to date</p>
-        <button className='parrafo-azul boton'>Suscribe</button>   
+    <div>
+      <section className='primer-block'>
+          <h1>Stay tuned for the <br /><i className='parrafo-azul'>latest updates</i></h1>
+          <h2>on Alzheimer's recent discoveries</h2>
+          <form action="">
+            <TextField fullWidth label="Enter your e-mail to be up to date" id="fullWidth" />  
+            <button className='parrafo-azul boton'>Suscribe</button> 
+          </form>
+      </section>
+      <div className="curveado">
+        <img src="/img2.svg" alt="hhh"/> 
+      </div>
+
+      <section className="segundo-bloque">
+        <div className='izquierda'>
+          <p><b>About 5.8 million in the United States who have alzheimer's disease </b></p>
+          <p>it accounts 60-80% of dementia cases</p>
+        </div>
         <h3>What is alzheimer?</h3>
-        <p><b>About 5.8 million in the United States who have alzheimer's disease </b>it accounts 60-80% of dementia cases</p>
         <p>Alzheimer's is a type of dementia that affects memory, thinking and behavior. Symptoms eventually grow severe enough to interfere with daily tasks. Alzheimer's is the most common cause of dementia, a general term for memory loss and other cognitive abilities serious enough to interfere with daily life.</p>
-        <h2>Latest News</h2>   
+      </section>
     </div>
+    
   )
 }
