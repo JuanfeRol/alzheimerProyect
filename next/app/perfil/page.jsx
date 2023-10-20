014dbe130e0a4a77b6c1f0bf9cbe45320fd72e6a
danihs2 <daniel.hachac@alumno.buap.mx>
2023-10-20 07:36:10 -0600

    Sb

---
+++ next/app/perfil/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import * as React from "react"; 
+
+export default function page(){
+    
+    return (
+        <section className="mai">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 172 172" style={{ fill: '#26e07f' }}>
+                <g fill="none" fillRule="nonzero" stroke="none" stroke-width="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                    <path d="M0,172v-172h172v172z" fill="none"></path>
+                    <g fill="#1fb141">
+                    <path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path>
+                    </g>
+                </g>
+            </svg>
+
+        </section>
+      );
+}
