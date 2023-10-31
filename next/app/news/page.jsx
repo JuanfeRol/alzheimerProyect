e797241fd23850488cc5e2ded2f5824ceab68c1a
danihs2 <daniel.hachac@alumno.buap.mx>
2023-10-30 19:52:59 -0600

    News Page

--- next/app/news/page.jsx
+++ next/app/news/page.jsx
@@ -7,21 +7,37 @@ export default function page(){
     
     return (
         <section className="main">
-            <h2>News Content</h2>
+            <h2>Latest News</h2>
+            <hr />
             <section className="latest_news">
+                <div className="first_card">
+                    <figure>
+                        <img src="/Images/Home1.jpg" alt="aaa" />
+                    </figure>
+                    <div className="contenido">
+                        <p className="date">12 Jun 23</p>
+                        <h3 className="new_title">How to sell drugs online</h3>
+                    </div>
+                </div>
                 <div className="news_section">
                     <div className="card">
-                        <p className="date">12 Jun 23</p>
-                        <h3 className="new_title">How to sell drugs online</h3>
+                        <figure>
+                            <img src="/Images/Home2.jpg" alt="aaa"/>
+                        </figure>
+                        <div className="contenido">
+                            <p className="date">12 Jun 23</p>
+                            <h3 className="new_title">How to sell drugs online</h3>
+                        </div>
                     </div>
                     <div className="card">
-                        <p className="date">12 Jun 23</p>
-                        <h3 className="new_title">How to sell drugs online</h3>
+                        <figure>
+                            <img src="/Images/Home2.jpg" alt="aaa" />
+                        </figure>
+                        <div className="contenido">
+                            <p className="date">12 Jun 23</p>
+                            <h3 className="new_title">How to sell drugs online</h3>
+                        </div>
                     </div>
-                    <div className="card">
-                        <p className="date">12 Jun 23</p>
-                        <h3 className="new_title">How to sell drugs online</h3>
-                    </div>                    
                 </div>
             </section>
         </section>
